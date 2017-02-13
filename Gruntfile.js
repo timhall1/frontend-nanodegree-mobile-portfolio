a3846f43d427fde8339ec2887ea0efd2d3af0b3b
Tim Hall <thall74@gatech.edu>
2017-02-12 16:54:50 -0800

    Minified anified and Inlined Files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,66 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    // Uglify all js files and place uglified versions in dist directory
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      my_target: {
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: '**/*.js',
+          dest: 'dist'
+        }]
+      }
+    },
+    // Minify all HTML files and place minified versions in dist directory
+    htmlmin: {
+      src: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'dist/index.html': 'src/index.html', // 'destination': 'source'
+          'dist/project-2048.html': 'src/project-2048.html',
+          'dist/project-mobile.html': 'src/project-mobile.html',
+          'dist/project-webperf.html': 'src/project-webperf.html',
+          'dist/views/pizza.html': 'src/views/pizza.html'
+        }  
+      }
+    },
+    // Inline all CSS into HTML files in dist directory
+    inline: {
+      dist: {
+        src: 'src/index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    // Minify all contents of a release directory and add a .min.css extension
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'src/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist/css',
+          ext: '.css'
+        }]
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-inline');
+
 
   // Default task(s).
+  grunt.registerTask('default', ['cssmin']);
   grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['htmlmin']);
+  grunt.registerTask('default', ['inline']);
+  
 
 };
